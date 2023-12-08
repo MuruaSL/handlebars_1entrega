190b300944f61e62543056c0a37414bc631544da
Sergio Leonardo Murua <86857851+MuruaSL@users.noreply.github.com>
2023-12-08 18:51:49 -0300

    se creo funcionalidad de hasheo en utils.js y se la utiliza en session router para guardar los nuesvos usuarios con contrasena hasheada , tambien se hizo la adaptacion para verificar que las contrasenas en el login sean correctas usando la otra funcion de utils.js

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -1,19 +1,23 @@
 import { Router } from "express";
 import UserModel from '../dao/models/user.model.js'
+import { createHash, isValidPassword } from "../utils.js";
 
 const sessionrouter = Router()
 
 sessionrouter.post('/signup', async (req, res) => {
-    const user = req.body;
+    const newUser = req.body;
+    
 
     // Verifica si el correo electrónico sigue el patrón para el rol de administrador
-    const isAdmin = /.*adminCoder@coder\.com.*/i.test(user.email);
+    const isAdmin = /.*adminCoder@coder\.com.*/i.test(newUser.email);
 
     // Asigna el rol correspondiente
-    user.role = isAdmin ? "admin" : "user";
+    newUser.role = isAdmin ? "admin" : "user";
 
     // Crea el usuario
-    await UserModel.create(user);
+    //primero hasheo
+    newUser.password = createHash(req.body.password)
+    await UserModel.create(newUser);
 
     res.redirect('/login');
 });
@@ -21,9 +25,11 @@ sessionrouter.post('/signup', async (req, res) => {
 
 sessionrouter.post('/login', async (req, res) => {
     const { email, password } = req.body
-    const user = await UserModel.findOne({ email, password })
+    const user = await UserModel.findOne({ email }).lean().exec()
     if(!user) return res.redirect('/login')
-
+    if (!isValidPassword(user,password)) {
+        return res.status(403).send({status:"error",error: "incorrect password"})
+    }
     req.session.user = user
 
     res.redirect('/profile')
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,21 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import bcrypt from 'bcrypt'
 
 const __filename = fileURLToPath( import.meta.url);
 const __dirname = dirname(__filename);
 
 
 export default __dirname;
+
+///////////////////////
+//   Bcrypt hasheo   //
+///////////////////////
+
+// Esta funcion helper, hasheara el password que le pasemos por parametro
+// la funcion devuelve el password hasheado
+export const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+// esta funcion helper compara si lo pasado por parametro es igual que lo almacenado
+// la funcion devuelve true false
+export const isValidPassword =(user, password) => bcrypt.compareSync(password, user.password)
+
