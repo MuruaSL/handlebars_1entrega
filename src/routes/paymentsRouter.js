f674fe1e0291ebeae6f4211ecfd262f586a43153
MuruaSL <leonardo-murua@hotmail.com.ar>
2024-03-04 10:46:09 -0300

    se agrego el envio de mail luego de c ualquiera de las posibles soluciones de mercado pago , aun falta configurar una forma en la cual extraer el mail de la persona que compra, Mp no te lo da  .

--- src/routes/paymentsRouter.js
+++ src/routes/paymentsRouter.js
@@ -2,6 +2,7 @@
 import { MercadoPagoConfig , Preference} from 'mercadopago';
 import config from '../config/config.js';
 import { Router } from 'express';
+import nodemailer from 'nodemailer'
 
 let access_token = config.accessToken
 // Agrega credenciales
@@ -14,46 +15,81 @@ router.get('/success-payment', async (req, res) => {
     res.render('success-payment');
     });
 
-router.get('/failure-payment', async (req, res) => {
-    res.render('failure-payment');
-});
+    router.get('/failure-payment', async (req, res) => {
+        // Enviar correo electrónico de fallo
+        await sendEmail('Fallo en la transacción', 'Tu transacción ha fallado.');
+    
+        res.render('failure-payment');
+    });
+    
+    router.get('/pending-payment', async (req, res) => {
+        // Enviar correo electrónico de pendiente
+        await sendEmail('Transacción pendiente', 'Tu transacción está pendiente.');
+    
+        res.render('pending-payment');
+    });
+    
 
-router.get('/pending-payment', async (req, res) => {
-    res.render('pending-payment');
-});
+    router.post('/', async (req, res) => {
+        try {
+            const { items } = req.body;
+            console.log("Datos recibidos en el servidor:", items);
+    
+            // Crear preferencia
+            const preference = new Preference(client);
+            const result = await preference.create({
+                body: {
+                    items: items,
+                    back_urls:{
+                        success:"https://handlebars1entrega-production.up.railway.app/create_preference/success-payment",
+                        failure:"https://handlebars1entrega-production.up.railway.app/create_preference/failure-payment",
+                        pending:"https://handlebars1entrega-production.up.railway.app/create_preference/pending-payment"
+                    },
+                    auto_return:"approved",
+                }
+            });
+            
+            // Enviar correo electrónico de éxito
+            await sendEmail('Éxito en la transacción', 'Tu transacción ha sido exitosa.');
+    
+            res.json({
+                id: result.id,
+            });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                error: "error al crear la preferencia"
+            });
+        }
+    });
 
-router.post('/', async (req, res) => {
-    try {
-        const { items } = req.body;
-        console.log("Datos recibidos en el servidor:", items); // Agrega este console.log para verificar los datos recibidos en el servidor
 
-        console.log(items); // Imprimir los items recibidos por consola
-        const preference = new Preference(client);
-        const result = await preference.create({
-            body: {
-                items: items,
-                back_urls:{
-                    success:"https://handlebars1entrega-production.up.railway.app/create_preference/success-payment",
-                    failure:"https://handlebars1entrega-production.up.railway.app/create_preference/failure-payment",
-                    pending:"https://handlebars1entrega-production.up.railway.app/create_preference/pending-payment"
-                },
-                auto_return:"approved",
 
-            }
-        });
-        console.log("Resultado de la creación de preferencia:", result); // Agrega este console.log para verificar el resultado de la creación de la preferencia
-
-        res.json({
-            id: result.id,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            error: "error al crear la preferencia"
-        });
+    // Configurar nodemailer con tus credenciales de correo electrónico
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: config.mailUser,
+      pass: config.mailPass
     }
-});
+  });
+// Función para enviar correos electrónicos
+async function sendEmail(subject, text) {
+    try {
+        const mailOptions = {
+            from: config.mailUser,
+            to: config.mailUser,////// cambiar luego para adaptar que el usuario ingrese su mail porque MP no te lo da 
+            subject: subject,
+            text: text
+        };
 
+        // Enviar el correo electrónico
+        await transporter.sendMail(mailOptions);
+        console.log('Correo electrónico enviado con éxito.');
+    } catch (error) {
+        console.error('Error al enviar el correo electrónico:', error);
+    }
+}
 
 
 
